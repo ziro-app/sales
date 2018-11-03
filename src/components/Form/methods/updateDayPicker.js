7a202315928a8b0322fb08a20b1e13d31e0b6817
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-02 21:00:15 -0700

    fix error when input_date was undefined

--- src/components/Form/methods/updateDayPicker.js
+++ src/components/Form/methods/updateDayPicker.js
@@ -1,4 +1,8 @@
-const updateDayPicker = that => input_name => input_date =>
-	that.setState({ [input_name]: input_date.toDateString() })
+const updateDayPicker = that => input_name => input_date => {
+	if (input_date)
+		that.setState({ [input_name]: input_date.toDateString() })
+	else
+		that.setState({ [input_name]: '' })
+}
 
 export default updateDayPicker
