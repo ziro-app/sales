0f8892fe212ee9524f110b7d06c380f447659fc4
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-06 21:05:24 -0800

    add basic styles

--- src/components/Form/components/InputForDayPicker.js
+++ src/components/Form/components/InputForDayPicker.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import { input } from '../styles'
+import { input, inputFocus } from '../styles'
 
 export default class InputForDayPicker extends Component {
 	render = () =>
-		<input onKeyPress={event => event.preventDefault()} style={input} {...this.props} />
+		<input 
+			onKeyPress={event => event.preventDefault()}
+			style={this.props.value ? inputFocus : input}
+			{...this.props}
+		/>
 }
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -2,8 +2,11 @@ export const
 
 form = {
 	maxWidth: '400px',
-	margin: '20px auto 60px',
-	padding: '0 15%'
+	margin: '0px auto 60px',
+	padding: '0px 15%',
+	display: 'grid',
+	justifyItems: 'center',
+	gridRowGap: '25px'
 },
 
 title = {
@@ -17,10 +20,17 @@ input = {
 	outline: 'none',
 	border: 'none',
 	borderRadius: '25px',
-	padding: '12px 25px',
+	width: '100%',
 	height: '22px',
+	padding: '12px 0px',
 	backgroundColor: 'rgba(48,62,77,0.025)',
 	fontSize: '1.6rem',
+	textIndent: '25px',
 	color: 'rgba(48,62,77,0.8)',
 	fontWeight: '700'
-}
+},
+
+inputFocus = Object.assign( {}, input, {
+	boxShadow: `0px 0px 10px 0px rgba(48,62,77,0.08), 0px 0px 20px 0px rgba(48,62,77,0.06), 
+	0px 0px 30px 0px rgba(48,62,77,0.04), 0px 0px 40px 0px rgba(48,62,77,0.02)`
+})
