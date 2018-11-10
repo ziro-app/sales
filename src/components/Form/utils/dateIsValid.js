051b855c732a2a4dbeb316479a08d38690c57ff2
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-09 18:20:15 -0800

    add remaining validation checks

--- src/components/Form/utils/dateIsValid.js
+++ src/components/Form/utils/dateIsValid.js
@@ -1,5 +1,5 @@
 /* checks whether a date is an instance of Date object and whether it's not NaN */
-/* when empty strings are passed to new Date it returns NaN but as an instance of Date */
+/* when strings are passed to new Date it returns NaN but as an instance of Date */
 /* a number or a string like '3' passes both tests, which is an incorrect edge case */
 
 const dateIsValid = date => new Date(date) instanceof Date && !isNaN(new Date(date))
--- src/components/Form/utils/formIsValid.js
+++ src/components/Form/utils/formIsValid.js
@@ -1,11 +1,28 @@
 import dateIsValid from './dateIsValid'
 
-const formIsValid = ({ start_date, representative, reseller, transaction_type, end_date }) => {
-	
-	const start_date_is_valid = dateIsValid(start_date)
-	const end_date_is_valid = dateIsValid(end_date)
-	
-	return start_date_is_valid && end_date_is_valid
+const formIsValid = ({
+	start_date,
+	representative,
+	reseller,
+	transaction_type,
+	end_date,
+	representatives,
+	resellers
+}) => {
+	if (representatives && resellers) {
+		/* check if each field contains valid values */
+		const start_date_is_valid = dateIsValid(start_date)
+		const representative_is_valid = representatives.filter( name => name === representative).length
+		const reseller_is_valid = resellers.filter( name => name === reseller).length
+		const transaction_type_is_valid = ['Online', 'Offline'].filter( type => type === transaction_type).length
+		const end_date_is_valid = dateIsValid(end_date)
+		/* return true if all are valid */
+		return start_date_is_valid
+			&& representative_is_valid
+			&& reseller_is_valid
+			&& transaction_type_is_valid
+			&& end_date_is_valid
+	}
 }
 
 export default formIsValid
