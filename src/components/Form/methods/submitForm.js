77ac1840fd977aa75e8aeb8c1e9533fbda961d76
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-09 17:49:07 -0800

    add validation for date fields

--- src/components/Form/methods/submitForm.js
+++ src/components/Form/methods/submitForm.js
@@ -2,7 +2,7 @@ import formIsValid from '../utils/formIsValid'
 import sendToBackend from '../utils/sendToBackend'
 
 const submitForm = that => async () => {
-	if (formIsValid) {
+	if (formIsValid(that.state)) {
 		try {
 			that.changeUiState('SUBMIT')
 			await sendToBackend(that.state)
--- src/components/Form/utils/formIsValid.js
+++ src/components/Form/utils/formIsValid.js
@@ -1,3 +1,11 @@
-const formIsValid = () => true
+import dateIsValid from './dateIsValid'
+
+const formIsValid = ({ start_date, representative, reseller, transaction_type, end_date }) => {
+	
+	const start_date_is_valid = dateIsValid(start_date)
+	const end_date_is_valid = dateIsValid(end_date)
+	
+	return start_date_is_valid && end_date_is_valid
+}
 
 export default formIsValid
