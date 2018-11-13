350cf10cd807de0d6f42f98e033eb014dae32948
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-12 21:59:38 -0800

    update error messages to be more specific

--- src/components/Form/utils/generateErrorMessages.js
+++ src/components/Form/utils/generateErrorMessages.js
@@ -6,9 +6,9 @@ const generateErrorMessages = ({
 	end_date_is_valid
 }) => {
 	const error_start_date = start_date_is_valid ? '' : 'Data inválida'
-	const error_representative = representative_is_valid ? '' : 'Opção inválida'
-	const error_reseller = reseller_is_valid ? '' : 'Opção inválida'
-	const error_transaction_type = transaction_type_is_valid ? '' : 'Opção inválida'
+	const error_representative = representative_is_valid ? '' : 'Assessor inválido'
+	const error_reseller = reseller_is_valid ? '' : 'Lojista inválido'
+	const error_transaction_type = transaction_type_is_valid ? '' : 'Tipo inválido'
 	const error_end_date = end_date_is_valid ? '' : 'Deve ser maior ou igual que data início'
 	return {
 		error_start_date,
