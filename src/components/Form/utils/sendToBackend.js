7512521396f28bacd22251615eef59df9a01f706
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 19:26:02 -0800

    test integration with backend

--- src/components/Form/utils/sendToBackend.js
+++ src/components/Form/utils/sendToBackend.js
@@ -1,3 +1,15 @@
-const sendToBackend = state => Promise.resolve('Sucesso')
+import { post } from 'axios'
+
+const sendToBackend = async state => {
+	try {
+		const result = await post(
+			'https://sales-backend.ziro.online/.netlify/functions/spreadsheet',
+			state
+		)
+		console.log(result)
+	} catch (error) {
+		console.log(error)
+	}
+}
 
 export default sendToBackend
