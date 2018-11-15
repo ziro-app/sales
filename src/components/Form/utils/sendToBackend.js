ebf860dc20b77c17ff444552e135323d87149381
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-15 15:24:37 -0800

    finish function to send form data to backend

--- src/components/Form/utils/sendToBackend.js
+++ src/components/Form/utils/sendToBackend.js
@@ -1,18 +1,17 @@
 import { post } from 'axios'
 
 const sendToBackend = async ({ start_date, representative, reseller, transaction_type, end_date }) => {
-	try {
-		const result = await post(`${process.env.BACKEND_URL}`, {
-			start_date,
-			representative,
-			reseller,
-			transaction_type,
-			end_date
-		})
-		console.log(result)
-	} catch (error) {
-		console.log(error)
-	}
+	const { data: { message } } = await post(`${process.env.BACKEND_URL}`, {
+		start_date,
+		representative,
+		reseller,
+		transaction_type,
+		end_date
+	})
+	if (message === 'SUCCESS')
+		console.log(message)
+	else
+		await Promise.reject(`Error submitting form. At sendToBackend: ${message}`)
 }
 
 export default sendToBackend
