220e63acf2993622a0f13d5ad5efdc9a446d5b0d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-16 20:32:01 -0800

    add react router and redirects file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Form from './Form/index'
+import NotFound from './NotFound/index'
 import ErrorBoundary from './ErrorBoundary/index'
 
 const App = () =>
 	<ErrorBoundary>
-		<Form />
+		<Router>
+			<Switch>
+				<Route exact path='/' component={Form} />
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
 	</ErrorBoundary>
 
 export default App
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const NotFound = () =>
+	<div>
+		<h1>Essa página não existe. Verifique a URL digitada</h1>
+	</div>
+
+export default NotFound
