52ad776e29f2c2243108d2d7bab57bc136d53453
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-16 18:08:54 -0800

    add function to translate months to portuguese

--- src/components/Form/utils/formatDate.js
+++ src/components/Form/utils/formatDate.js
@@ -1,4 +1,21 @@
+const translateMonth = month => {
+	switch (month) {
+		case 'Jan': return 'Jan'
+		case 'Feb': return 'Fev'
+		case 'Mar': return 'Mar'
+		case 'Apr': return 'Abr'
+		case 'May': return 'Mai'
+		case 'Jun': return 'Jun'
+		case 'Jul': return 'Jul'
+		case 'Aug': return 'Ago'
+		case 'Sep': return 'Set'
+		case 'Oct': return 'Out'
+		case 'Nov': return 'Nov'
+		case 'Dec': return 'Dez'
+	}
+}
+
 const formatDate = date =>
-	`${date.toString().substr(8,2)}/${date.toString().substr(4,3)}/${date.getFullYear()}`
+	`${date.toString().substr(8,2)}/${translateMonth(date.toString().substr(4,3))}/${date.getFullYear()}`
 
 export default formatDate
