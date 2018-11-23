1df5777c9d3eb10da082a263263b42e68f35fbde
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-22 20:04:28 -0800

    change Form component's name and add new route

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Form from './Form/index'
+import Sale from './Sale/index'
+import Dashboard from './Dashboard/index'
 import NotFound from './NotFound/index'
 import ErrorBoundary from './ErrorBoundary/index'
 
@@ -8,7 +9,8 @@ const App = () =>
 	<ErrorBoundary>
 		<Router>
 			<Switch>
-				<Route exact path='/' component={Form} />
+				<Route exact path='/' component={Sale} />
+				<Route path='/dashboard' component={Dashboard} />
 				<Route component={NotFound} />
 			</Switch>
 		</Router>
---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+	render = () =>
+		<p>Dashboard</p>
+}
