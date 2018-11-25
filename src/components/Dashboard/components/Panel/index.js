624f548138326e6c5a9c830f341df362506d348a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-24 21:33:15 -0800

    call filterAndSort function from component

--- src/components/Dashboard/components/Panel/index.js
+++ src/components/Dashboard/components/Panel/index.js
@@ -2,23 +2,19 @@ import React from 'react'
 import filterAndSort from '../../utils/filterAndSort'
 import { container, title, row, reseller } from './styles'
 
-const Panel = ({ sales }) => {
-	filterAndSort(sales)
-	return (
-		<ul style={container}>
-			<li style={title}>
-				<span>Fim</span>
-				<span>Assessor</span>
-				<span>Lojista</span>
+const Panel = ({ sales }) =>
+	<ul style={container}>
+		<li style={title}>
+			<span>Fim</span>
+			<span>Assessor</span>
+			<span>Lojista</span>
+		</li>
+		{filterAndSort(sales).map( ({ id, fim, assessor, lojista }) =>
+			<li style={row} key={id}>
+				<span>{fim}</span>
+				<span>{assessor}</span>
+				<span style={reseller}>{lojista}</span>
 			</li>
-			{sales.map( sale =>
-				<li style={row} key={sale[0]}>
-					<span>{sale[4].substr(0,6)}</span>
-					<span>{sale[2]}</span>
-					<span style={reseller}>{sale[3]}</span>
-				</li>
-			)}
-		</ul>
-	)
-}
+		)}
+	</ul>
 export default Panel
--- src/components/Dashboard/utils/filterAndSort.js
+++ src/components/Dashboard/utils/filterAndSort.js
@@ -1,12 +1,17 @@
 const filterAndSort = sales => {
 	const filtered = sales.filter( sale => sale[5] === 'Aberto' )
 	const sorted = filtered.sort( (a,b) => new Date(a[4]) - new Date(b[4]) || (a[2] < b[2] ? -1 : 1) )
-	const table = sorted.map( sale => ({
+	const simplified = sorted.map( sale => {
+		sale[4] = sale[4].substr(0,6)
+		return sale
+	})
+	const table = simplified.map( sale => ({
+		id: sale[0],
 		fim: sale[4],
 		assessor: sale[2],
 		lojista: sale[3]
 	}))
-	console.log(table)
+	return table
 }
 
 export default filterAndSort
