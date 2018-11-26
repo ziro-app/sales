b9eb7dbe04ed702021f240f46832eab8c9774167
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-25 18:54:55 -0800

    finish designing fixed addNew button

--- src/components/Dashboard/components/AddNew/index.js
+++ src/components/Dashboard/components/AddNew/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import AddNewSvg from './AddNewSvg'
+import { button } from './styles'
 
 const AddNew = () =>
-	<AddNewSvg />
+	<Link to='/'>
+		<div style={button}>
+			<AddNewSvg size={27} color={'rgba(48,62,77,1)'} />
+		</div>
+	</Link>
 
 export default AddNew
---
+++ src/components/Dashboard/components/AddNew/styles.js
@@ -0,0 +1,13 @@
+export const
+
+button = {
+	position: 'fixed',
+	bottom: '2%',
+	right: '5%',
+	display: 'grid',
+	padding: '15px',
+	borderRadius: '50%',
+	backgroundColor: 'rgb(247,247,247)',
+	boxShadow: `0px 0px 10px 0px rgba(48,62,77,0.08), 0px 0px 20px 0px rgba(48,62,77,0.06), 
+	0px 0px 30px 0px rgba(48,62,77,0.04), 0px 0px 40px 0px rgba(48,62,77,0.02)`
+}
