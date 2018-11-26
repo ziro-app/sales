5fca3532116b22ed03eeb0fbeef28f9d741c7c1d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-25 18:02:38 -0800

    add svg icon and start designing new component

---
+++ src/components/Dashboard/components/AddNew/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import AddNewSvg from './AddNewSvg'
+
+const AddNew = () =>
+	<AddNewSvg />
+
+export default AddNew
--- src/components/Dashboard/components/Panel/index.js
+++ src/components/Dashboard/components/Panel/index.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import AddNew from '../AddNew/index'
 import filterAndSort from './utils/filterAndSort'
 import { container, status, header, representative, row, reseller } from './styles'
 
 const Panel = ({ sales }) =>
-	<ul style={container}>
-		<h1 style={status}>Abertos</h1>
-		<li style={header}>
-			<span>Fim</span>
-			<span style={representative}>Assessor</span>
-			<span>Lojista</span>
-		</li>
-		{filterAndSort(sales).map( ({ id, fim, assessor, lojista }) =>
-			<li style={row} key={id}>
-				<span>{fim}</span>
-				<span>{assessor}</span>
-				<span style={reseller}>{lojista}</span>
+	<Fragment>	
+		<ul style={container}>
+			<h1 style={status}>Abertos</h1>
+			<li style={header}>
+				<span>Fim</span>
+				<span style={representative}>Assessor</span>
+				<span>Lojista</span>
 			</li>
-		)}
-	</ul>
+			{filterAndSort(sales).map( ({ id, fim, assessor, lojista }) =>
+				<li style={row} key={id}>
+					<span>{fim}</span>
+					<span>{assessor}</span>
+					<span style={reseller}>{lojista}</span>
+				</li>
+			)}
+		</ul>
+		<AddNew />
+	</Fragment>
 
 export default Panel
