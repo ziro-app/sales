b2661a6f60c3923b7c8a213d69e28bea9cb811f2
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-12-18 08:45:52 -0800

    add basic styles and html structure to SaleView

--- src/components/SaleView/index.js
+++ src/components/SaleView/index.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import Header from '../Header/index'
+import { container, row, label } from './styles'
 
 const SaleView = ({ match: { path, params: { id } } }) =>
 	<Header title={`Atendimento ${id}`} path={path}>
-		<div>{id}</div>
+		<div style={container}>
+			<div style={row}>
+				<span style={label}>Início</span>
+				<span>01/01/2019</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Assessor</span>
+				<span>Rubia</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Lojista</span>
+				<span>CLAUDIA CAMPELO DE OLIVEIRA</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Categoria</span>
+				<span>Troca</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Tipo</span>
+				<span>Online</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Fim</span>
+				<span>02/01/2019</span>
+			</div>
+			<div style={row}>
+				<span style={label}>Status</span>
+				<span>Aberto</span>
+			</div>
+		</div>
 	</Header>
 
 export default SaleView
---
+++ src/components/SaleView/styles.js
@@ -0,0 +1,20 @@
+export const
+
+container = {
+	display: 'grid',
+	gridRowGap: '10px',
+	width: '100%',
+	textAlign: 'left',
+	fontSize: '1.4rem',
+	color: 'rgba(48, 62, 77, 0.8)'
+},
+
+row = {
+	display: 'grid',
+	gridTemplateColumns: '100px 1fr',
+	gridColumnGap: '5px'
+},
+
+label = {
+	fontWeight: '600'
+}
