b5dba9d9e9a08f6072c1e150d686aa3fa7a234dd
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-12-19 21:10:09 -0800

    add basic structure and styling of select field to edit status

--- src/components/SaleView/index.js
+++ src/components/SaleView/index.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import Header from '../Header/index'
-import { container, row, label } from './styles'
+import { container, row, label, data, select } from './styles'
 
 const SaleView = ({ match: { path, params: { id } } }) =>
 	<Header title={`Atendimento ${id}`} path={path}>
 		<div style={container}>
 			<div style={row}>
 				<span style={label}>Início</span>
-				<span>01/01/2019</span>
+				<span style={data}>01/01/2019</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Assessor</span>
-				<span>Rubia</span>
+				<span style={data}>Rubia</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Lojista</span>
-				<span>CLAUDIA CAMPELO DE OLIVEIRA</span>
+				<span style={data}>CLAUDIA CAMPELO DE OLIVEIRA</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Categoria</span>
-				<span>Troca</span>
+				<span style={data}>Troca</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Tipo</span>
-				<span>Online</span>
+				<span style={data}>Online</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Fim</span>
-				<span>02/01/2019</span>
+				<span style={data}>02/01/2019</span>
 			</div>
 			<div style={row}>
 				<span style={label}>Status</span>
-				<span>Aberto</span>
+				<select style={select}>
+					{['Aberto', 'Entregue'].map( (option, index) =>
+						<option value={option} key={index}>{option}</option>
+					)}
+				</select>
 			</div>
 		</div>
 	</Header>
--- src/components/SaleView/styles.js
+++ src/components/SaleView/styles.js
@@ -12,9 +12,27 @@ container = {
 row = {
 	display: 'grid',
 	gridTemplateColumns: '100px 1fr',
+	alignItems: 'center',
 	gridColumnGap: '5px'
 },
 
 label = {
 	fontWeight: '600'
-}
+},
+
+data = {
+	padding: '5px 20px'	
+},
+
+select = Object.assign({}, data, {
+	WebkitAppearance: 'none',
+	MozAppearance: 'none',
+	outline: 'none',
+	border: 'none',
+	borderRadius: '20px',
+	fontSize: '1.4rem',
+	color: 'rgba(48, 62, 77, 0.8)',
+	backgroundColor: 'rgba(48,62,77,0.024)',
+	boxShadow: `0px 3px 10px 0px rgba(48,62,77,0.10), 0px 3px 20px 0px rgba(48,62,77,0.08), 
+	0px 3px 30px 0px rgba(48,62,77,0.06), 0px 3px 40px 0px rgba(48,62,77,0.04)`
+})
