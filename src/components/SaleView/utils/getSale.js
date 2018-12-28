af83f97e1f11c30bac38e52448935762cabb27fc
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-12-27 20:39:48 -0800

    fix date string lenght

--- src/components/SaleView/utils/getSale.js
+++ src/components/SaleView/utils/getSale.js
@@ -2,12 +2,12 @@ const getSale = (id, sales) => {
 	const filtered = sales.filter(sale => sale[0] === id).pop()
 	if (filtered && filtered.length)
 		return {
-			inicio: filtered[1],
+			inicio: filtered[1].substr(0,6),
 			assessor: filtered[2],
 			lojista: filtered[3],
 			categoria: filtered[4],
 			tipo: filtered[5],
-			fim: filtered[6],
+			fim: filtered[6].substr(0,6),
 			status: filtered[7]
 		}
 	else return {}
