76fd6b64bc09722e98c6713c4e69b7fd0c83c2bf
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-10 17:48:19 -0800

    prevent non0existent routes from being rendered

--- src/components/SaleView/components/View/index.js
+++ src/components/SaleView/components/View/index.js
@@ -5,45 +5,49 @@ import getSale from '../../utils/getSale'
 import { container, row, label, data, select, link, edit } from './styles'
 
 const View = ({ id, sales, statuses }) => {
-	const { inicio, assessor, lojista, categoria, tipo, fim, status } = getSale(id, sales)
+	const { found, inicio, assessor, lojista, categoria, tipo, fim, status } = getSale(id, sales)
+	if (found)
+		return (
+			<div style={container}>
+				<div style={row}>
+					<span style={label}>Início</span>
+					<span style={data}>{inicio}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Assessor</span>
+					<span style={data}>{assessor}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Lojista</span>
+					<span style={data}>{lojista}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Categoria</span>
+					<span style={data}>{categoria}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Tipo</span>
+					<span style={data}>{tipo}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Fim</span>
+					<span style={data}>{fim}</span>
+				</div>
+				<div style={row}>
+					<span style={label}>Status</span>
+					<select style={select} value={status}>
+						{statuses.map( (option, index) =>
+							<option value={option} key={index}>{option}</option>
+						)}
+					</select>
+				</div>
+				<Link style={link} to={`/atendimentos/${id}/editar`}>
+					<input style={edit} type='submit' value='Editar' />
+				</Link>
+			</div>
+		)
 	return (
-		<div style={container}>
-			<div style={row}>
-				<span style={label}>Início</span>
-				<span style={data}>{inicio}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Assessor</span>
-				<span style={data}>{assessor}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Lojista</span>
-				<span style={data}>{lojista}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Categoria</span>
-				<span style={data}>{categoria}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Tipo</span>
-				<span style={data}>{tipo}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Fim</span>
-				<span style={data}>{fim}</span>
-			</div>
-			<div style={row}>
-				<span style={label}>Status</span>
-				<select style={select} value={status}>
-					{statuses.map( (option, index) =>
-						<option value={option} key={index}>{option}</option>
-					)}
-				</select>
-			</div>
-			<Link style={link} to={`/atendimentos/${id}/editar`}>
-				<input style={edit} type='submit' value='Editar' />
-			</Link>
-		</div>
+		<div>Atendimento não encontrado</div>
 	)
 }
 
--- src/components/SaleView/utils/getSale.js
+++ src/components/SaleView/utils/getSale.js
@@ -2,6 +2,7 @@ const getSale = (id, sales) => {
 	const filtered = sales.filter(sale => sale[0] === id).pop()
 	if (filtered && filtered.length)
 		return {
+			found: true,
 			inicio: filtered[1].substr(0,6),
 			assessor: filtered[2],
 			lojista: filtered[3],
@@ -10,7 +11,7 @@ const getSale = (id, sales) => {
 			fim: filtered[6].substr(0,6),
 			status: filtered[7]
 		}
-	else return {}
+	else return { found: false }
 }
 
 export default getSale
