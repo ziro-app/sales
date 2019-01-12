885c4e2fae3b115285759ea04680f15fa90eb45d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-11 19:11:13 -0800

    add conditional rendering

--- src/components/SaleView/components/View/Select/index.js
+++ src/components/SaleView/components/View/Select/index.js
@@ -3,23 +3,26 @@ import AlertSvg from './AlertSvg'
 import Spinner from './Spinner/index'
 import { row, label, select, error } from './styles'
 
-const Select = ({ uiState, selectValue, updateParentAndSheet, statuses }) =>
-	<div style={row}>
-		<span style={label}>Status</span>
-		<select style={select} value={selectValue} onChange={updateParentAndSheet}>
-			{statuses.map( (option, index) =>
-				<option value={option} key={index}>{option}</option>
-			)}
-		</select>
-		{
-			uiState === 'fetched'
-			? <label style={error}>
-				{'Enviando...' ? <Spinner size={'20px'} /> : null} &nbsp; {'Enviando...'}
-			  </label>
-			: <label style={error}>
-				{'Erro ao enviar. Tente novamente' ? <AlertSvg size={16} /> : null} &nbsp; {'Erro ao enviar. Tente novamente'}
-			  </label>
-		}
-	</div>
+const Select = ({ uiState, selectValue, updateParentAndSheet, statuses }) => {
+	const componentsToRender = {
+		default: null,
+		submitting: <span><Spinner size={'20px'} />{'Enviando...'}</span>,
+		submitted: <span>Status atualizado!</span>,
+		error_submitting: <span><AlertSvg size={16} />{'Erro ao enviar. Tente novamente'}</span>
+	}
+	return (
+		<div style={row}>
+			<span style={label}>Status</span>
+			<select style={select} value={selectValue} onChange={updateParentAndSheet}>
+				{statuses.map( (option, index) =>
+					<option value={option} key={index}>{option}</option>
+				)}
+			</select>
+			<label style={error}>
+				{componentsToRender['error_submitting']}
+			</label>
+		</div>
+	)
+}
 
 export default Select
