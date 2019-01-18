ca5de49f1dcc00781a0f761dd779047bdf28b1f4
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-17 17:45:13 -0800

    add sale id to data sent to backend

--- src/components/Sale/methods/submitForm.js
+++ src/components/Sale/methods/submitForm.js
@@ -3,14 +3,15 @@ import generateErrorMessages from '../utils/generateErrorMessages'
 import sendToBackend from '../utils/sendToBackend'
 
 const submitForm = that => async () => {
-	const action = that.props.match.params.id ? 'EDIT' : 'CREATE'
+	const sale = that.props.match.params.id
+	const action = sale ? 'EDIT' : 'CREATE'
 	const { formIsValid, ...fieldsAreValid } = validateForm(that.state)
 	that.setState(generateErrorMessages(fieldsAreValid))
 	that.changeUiState('INPUT')
 	if (formIsValid) {
 		try {
 			that.changeUiState('SUBMIT')
-			await sendToBackend(that.state, action)
+			await sendToBackend(that.state, sale, action)
 			that.changeUiState('SUCCESS')
 			that.setState({
 				start_date: '',
--- src/components/Sale/utils/sendToBackend.js
+++ src/components/Sale/utils/sendToBackend.js
@@ -1,8 +1,9 @@
 import { post } from 'axios'
 
-const sendToBackend = async ({ start_date, representative, reseller, category, type, end_date }, action) => {
+const sendToBackend = async ({ start_date, representative, reseller, category, type, end_date }, sale, action) => {
 	const url = action === 'CREATE' ? process.env.CREATE_ROW_URL : process.env.EDIT_ROW_URL
 	const { data: { message } } = await post(`${url}`, {
+		sale,
 		start_date,
 		representative,
 		reseller,
