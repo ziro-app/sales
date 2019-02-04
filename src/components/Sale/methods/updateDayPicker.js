9757572d02eb2fcc546aa75e23702c7ecb3eb887
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-03 20:37:30 -0800

    fix problem comparing dates

--- src/components/Sale/methods/updateDayPicker.js
+++ src/components/Sale/methods/updateDayPicker.js
@@ -1,6 +1,6 @@
 const updateDayPicker = that => input_name => input_date => {
 	if (input_date) {
-		that.setState({ [input_name]: input_date })
+		that.setState({ [input_name]: new Date(new Date(input_date).setHours(0,0,0)) })
 		that.changeUiState('INPUT')
 	}
 	else {
