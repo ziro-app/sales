08c2d068ef63b759f6d873d06f2a3373fc2a3ad9
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-05 09:41:41 -0800

    render 'not allowed' message to editing routes in certain sale statuses

--- src/components/Sale/components/Form/index.js
+++ src/components/Sale/components/Form/index.js
@@ -20,81 +20,81 @@ import { body, notAllowed, input } from './styles'
 
 const Form = ({ id, state, updateDropdown, updateDayPicker, updateRadio, submitForm }) =>
 	<div style={body}>
-		{id && state.status === 'Em trânsito'
-			?
-				<div style={notAllowed}>Atendimentos com status 'Em trânsito' não podem ser modificados</div>
-			:
-				<Fragment>
-					<ErrorMessage message={state.error_start_date}>
-						{id && state.status !== 'Escolhendo'
-							?
-								<input
-									style={input}
-									placeholder={formatDate(state.start_date)}
-									disabled={true}
-								/>
-							:
-								<DayPickerInput
-									component={InputForDayPicker}
-									placeholder='Data início'
-									value={state.start_date}
-									onDayChange={updateDayPicker('start_date')}
-									formatDate={formatDate}
-									dayPickerProps={dayPickerProps}
-								/>
-						}
-					</ErrorMessage>
-					<ErrorMessage message={state.error_representative}>
-						<Dropdown
-							name='representative'
-							placeholder='Assessor'
-							options={state.representatives}
-							value={state.representative}
-							updateParent={updateDropdown}
+		{id && state.status.match(/(Em trânsito)|(Entregue)|(Cancelado)/)
+		?
+			<div style={notAllowed}>Esse atendimento foi despachado ou cancelado e não pode mais ser editado</div>
+		:
+			<Fragment>
+				<ErrorMessage message={state.error_start_date}>
+					{id && state.status !== 'Escolhendo'
+					?
+						<input
+							style={input}
+							placeholder={formatDate(state.start_date)}
+							disabled={true}
 						/>
-					</ErrorMessage>
-					<ErrorMessage message={state.error_reseller}>
-						<Dropdown
-							name='reseller'
-							placeholder='Lojista'
-							options={state.resellers}
-							value={state.reseller}
-							updateParent={updateDropdown}
-						/>
-					</ErrorMessage>
-					<ErrorMessage message={state.error_category}>	
-						<Radio
-							name='category'
-							options={['Venda','Troca']}
-							icons={[<SaleSvg />,<ReturnSvg />]}
-							value={state.category}
-							updateParent={updateRadio}
-						/>
-					</ErrorMessage>	
-					<ErrorMessage message={state.error_type}>	
-						<Radio
-							name='type'
-							options={['Online','Offline']}
-							icons={[<WifiOnSvg />,<WifiOffSvg />]}
-							value={state.type}
-							updateParent={updateRadio}
-						/>
-					</ErrorMessage>
-					<ErrorMessage message={state.error_end_date}>
+					:
 						<DayPickerInput
 							component={InputForDayPicker}
-							placeholder='Data fim'
-							value={state.end_date}
-							onDayChange={updateDayPicker('end_date')}
+							placeholder='Data início'
+							value={state.start_date}
+							onDayChange={updateDayPicker('start_date')}
 							formatDate={formatDate}
 							dayPickerProps={dayPickerProps}
 						/>
-					</ErrorMessage>
-					<Submit
-						submitForm={submitForm}
-						uiState={state.uiState}
+					}
+				</ErrorMessage>
+				<ErrorMessage message={state.error_representative}>
+					<Dropdown
+						name='representative'
+						placeholder='Assessor'
+						options={state.representatives}
+						value={state.representative}
+						updateParent={updateDropdown}
 					/>
-				</Fragment>
+				</ErrorMessage>
+				<ErrorMessage message={state.error_reseller}>
+					<Dropdown
+						name='reseller'
+						placeholder='Lojista'
+						options={state.resellers}
+						value={state.reseller}
+						updateParent={updateDropdown}
+					/>
+				</ErrorMessage>
+				<ErrorMessage message={state.error_category}>	
+					<Radio
+						name='category'
+						options={['Venda','Troca']}
+						icons={[<SaleSvg />,<ReturnSvg />]}
+						value={state.category}
+						updateParent={updateRadio}
+					/>
+				</ErrorMessage>	
+				<ErrorMessage message={state.error_type}>	
+					<Radio
+						name='type'
+						options={['Online','Offline']}
+						icons={[<WifiOnSvg />,<WifiOffSvg />]}
+						value={state.type}
+						updateParent={updateRadio}
+					/>
+				</ErrorMessage>
+				<ErrorMessage message={state.error_end_date}>
+					<DayPickerInput
+						component={InputForDayPicker}
+						placeholder='Data fim'
+						value={state.end_date}
+						onDayChange={updateDayPicker('end_date')}
+						formatDate={formatDate}
+						dayPickerProps={dayPickerProps}
+					/>
+				</ErrorMessage>
+				<Submit
+					submitForm={submitForm}
+					uiState={state.uiState}
+				/>
+			</Fragment>
 		}
 	</div>
 
