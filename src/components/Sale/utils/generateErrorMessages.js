a9f7491ff6ba68d21e930587267fc2ce87253546
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-08 18:50:40 -0800

    add error validation to new form field

--- src/components/Sale/utils/generateErrorMessages.js
+++ src/components/Sale/utils/generateErrorMessages.js
@@ -6,7 +6,8 @@ const generateErrorMessages = ({
 	type_is_valid,
 	end_date_is_valid,
 	time_is_valid,
-	shipping_is_valid
+	shipping_is_valid,
+	packaging_is_valid
 }) => {
 	const error_start_date = start_date_is_valid ? '' : 'Data inválida'
 	const error_representative = representative_is_valid ? '' : 'Assessor inválido'
@@ -16,6 +17,7 @@ const generateErrorMessages = ({
 	const error_end_date = end_date_is_valid ? '' : 'Deve ser maior ou igual que data início'
 	const error_time = time_is_valid ? '' : 'Deve ser entre 08:00 e 17:00'
 	const error_shipping = shipping_is_valid ? '' : 'Transporte inválido'
+	const error_packaging = packaging_is_valid ? '' : 'Opção de fardo inválida'
 	return {
 		error_start_date,
 		error_representative,
@@ -24,7 +26,8 @@ const generateErrorMessages = ({
 		error_type,
 		error_end_date,
 		error_time,
-		error_shipping
+		error_shipping,
+		error_packaging
 	}
 }
 
--- src/components/Sale/utils/validateForm.js
+++ src/components/Sale/utils/validateForm.js
@@ -9,11 +9,13 @@ const validateForm = ({
 	end_date,
 	time,
 	shipping,
+	packaging,
 	representatives,
 	resellers,
-	shipping_options
+	shipping_options,
+	packaging_options
 }) => {
-	if (representatives && resellers && shipping_options) {
+	if (representatives && resellers && shipping_options && packaging_options) {
 		/* check if each field contains valid values */
 		const start_date_is_valid = dateIsValid(start_date)
 		const representative_is_valid = optionIsValid(representatives, representative)
@@ -23,6 +25,7 @@ const validateForm = ({
 		const end_date_is_valid = new Date(end_date) >= new Date(start_date)
 		const time_is_valid = timeIsValid(time)
 		const shipping_is_valid = optionIsValid(shipping_options, shipping)
+		const packaging_is_valid = optionIsValid(packaging_options, packaging)
 		const formIsValid = start_date_is_valid
 			&& representative_is_valid
 			&& reseller_is_valid
@@ -31,6 +34,7 @@ const validateForm = ({
 			&& end_date_is_valid
 			&& time_is_valid
 			&& shipping_is_valid
+			&& packaging_is_valid
 		/* return the status of each field and if all are valid */
 		return {
 			formIsValid,
@@ -41,7 +45,8 @@ const validateForm = ({
 			type_is_valid,
 			end_date_is_valid,
 			time_is_valid,
-			shipping_is_valid
+			shipping_is_valid,
+			packaging_is_valid
 		}
 	}
 }
