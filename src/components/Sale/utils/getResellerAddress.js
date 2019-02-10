4c9ca191ccc8cdb8fa4eb6ff33a315b026427163
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-09 19:21:48 -0800

    prevent undefined addresses

--- src/components/Sale/utils/getResellerAddress.js
+++ src/components/Sale/utils/getResellerAddress.js
@@ -2,7 +2,11 @@ const getResellerAddress = (addresses, reseller) => {
 	if (reseller) {
 		const [ resellerAddress ] = addresses.filter( address => Boolean(address[reseller]) )
 		const [ favorites ] = addresses.filter( address => Boolean(address['FAVORITOS']) )
-		return [ ...Object.values(resellerAddress), ...favorites['FAVORITOS'] ]
+		if (resellerAddress && favorites)
+			return [ ...Object.values(resellerAddress), ...favorites['FAVORITOS'] ]
+		if (favorites)
+			return [ ...favorites['FAVORITOS'] ]
+		return []
 	}
 	return []
 }
