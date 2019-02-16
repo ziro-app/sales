04c0a54a5e125d12afeb7a2debf2b235f2b514ee
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-16 15:58:12 -0800

    further refactor createRow to simplify code

--- src/components/Sale/utils/sendToBackend.js
+++ src/components/Sale/utils/sendToBackend.js
@@ -1,18 +1,10 @@
 import { post } from 'axios'
 
-const sendToBackend = async ({ start_date, representative, reseller, category, type, end_date }, sale, action) => {
+const sendToBackend = async (dataToSend, sale, action) => {
 	const url = action === 'CREATE' ? process.env.CREATE_ROW_URL : process.env.EDIT_ROW_URL
-	const { data: message } = await post(`${url}`, {
-		sale,
-		start_date,
-		representative,
-		reseller,
-		category,
-		type,
-		end_date
-	})
+	const { data: message } = await post(`${url}`, dataToSend)
 	if (message !== 'Success')
-		Promise.reject(`Error submitting form. At sendToBackend: ${message}`)
+		await Promise.reject(`Error submitting form. At sendToBackend: ${message}`)
 }
 
 export default sendToBackend
