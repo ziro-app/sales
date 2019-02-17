a5be048f70342ba127f004ae732ed3e7cfcdf367
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-16 16:27:33 -0800

    refacto sendToBackend

--- src/components/Sale/utils/sendToBackend.js
+++ src/components/Sale/utils/sendToBackend.js
@@ -1,8 +1,35 @@
 import { post } from 'axios'
 
-const sendToBackend = async (dataToSend, sale, action) => {
+const sendToBackend = async ({ 
+		start_date,
+		representative,
+		reseller,
+		category,
+		type,
+		end_date,
+		time,
+		shipping,
+		address,
+		packaging,
+		invoice,
+		comments
+	}, sale, action) => {
 	const url = action === 'CREATE' ? process.env.CREATE_ROW_URL : process.env.EDIT_ROW_URL
-	const { data: message } = await post(`${url}`, dataToSend)
+	const { data: message } = await post(`${url}`, {
+		sale,
+		start_date,
+		representative,
+		reseller,
+		category,
+		type,
+		end_date,
+		time,
+		shipping,
+		address,
+		packaging,
+		invoice,
+		comments
+	})
 	if (message !== 'Success')
 		await Promise.reject(`Error submitting form. At sendToBackend: ${message}`)
 }
