d5ca1bee10490a30011dda500debd3fe57aaf768
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-18 20:47:11 -0800

    fix edge case where status was being updated in front end even if there was error in backend

--- src/components/SaleView/methods/saveStatus.js
+++ src/components/SaleView/methods/saveStatus.js
@@ -2,11 +2,11 @@ import sendToBackend from '../utils/sendToBackend'
 
 const saveStatus = that => async ({ target: { value } }) => {
 	if (that.state.saleIsComplete || value === 'Cancelado') {
-		that.setState({ status: value, errorIsComplete: '' })
 		try {
 			that.changeUiState('SUBMIT')
 			await sendToBackend(that.props.match.params.id, value)
 			that.changeUiState('SUCCESS')
+			that.setState({ status: value, errorIsComplete: '' })
 		} catch (error) {
 			console.log(error)
 			that.changeUiState('ERROR')
