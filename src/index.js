5d5f2cc37e921fbf54567606e53edeb8a1f1f03f
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-27 18:07:15 -0800

    change components root folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/App/index'
+import App from './App/index'
 import './index.css'
 import 'react-day-picker/lib/style.css'
 
