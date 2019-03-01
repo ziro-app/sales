961f3bd4fb6fabac799c167ecbaeb3ccd17648ef
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-28 19:08:25 -0800

    minor refactoring on svg icons

--- src/Dashboard/components/AddNew/AddNewSvg.js
+++ src/Dashboard/components/AddNew/AddNewSvg.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const AddNewSvg = ({ size, color }) =>
-	<svg width={size} height={size} viewBox={'0 0 24 24'} fill={'none'} stroke={color} strokeWidth={'2'} strokeLinecap={'round'} strokeLinejoin={'round'}>
-		<line x1={'12'} y1={'5'} x2={'12'} y2={'19'}></line>
-		<line x1={'5'} y1={'12'} x2={'19'} y2={'12'}></line>
+	<svg width={size} height={size} viewBox='0 0 24 24' fill='none' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+		<line x1='12' y1='5' x2='12' y2='19'></line>
+		<line x1='5' y1='12' x2='19' y2='12'></line>
 	</svg>
 
 AddNewSvg.defaultProps = {
@@ -13,8 +13,8 @@ AddNewSvg.defaultProps = {
 }
 
 AddNewSvg.propTypes = {
-	size: PropTypes.number.isRequired,
-	color: PropTypes.string.isRequired
+	size: PropTypes.number,
+	color: PropTypes.string
 }
 
 export default AddNewSvg
--- src/SaleCreate/components/Form/ErrorMessage/AlertSvg.js
+++ src/SaleCreate/components/Form/ErrorMessage/AlertSvg.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const AlertSvg = ({ size, color }) =>
-	<svg width={size} height={size} viewBox={'0 0 24 24'} fill={'none'} stroke={color} strokeWidth={'2'} strokeLinecap={'round'} strokeLinejoin={'round'}>
-		<circle cx={'12'} cy={'12'} r={'10'}></circle>
-		<line x1={'12'} y1={'8'} x2={'12'} y2={'12'}></line>
-		<line x1={'12'} y1={'16'} x2={'12'} y2={'16'}></line>
+	<svg width={size} height={size} viewBox='0 0 24 24' fill='none' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+		<circle cx='12' cy='12' r='10'></circle>
+		<line x1='12' y1='8' x2='12' y2='12'></line>
+		<line x1='12' y1='16' x2='12' y2='16'></line>
 	</svg>
 
 AlertSvg.defaultProps = {
@@ -14,8 +14,8 @@ AlertSvg.defaultProps = {
 }
 
 AlertSvg.propTypes = {
-	size: PropTypes.number.isRequired,
-	color: PropTypes.string.isRequired
+	size: PropTypes.number,
+	color: PropTypes.string
 }
 
 export default AlertSvg
