f6c9363fad074b36b072ddeacecaa68d4aee88cf
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-04 19:59:39 -0800

    minor fix

--- src/SaleCreate/methods/updateDayPicker.js
+++ src/SaleCreate/methods/updateDayPicker.js
@@ -1,12 +1,9 @@
 const updateDayPicker = that => input_name => input_date => {
-	if (input_date) {
+	if (input_date)
 		that.setState({ [input_name]: new Date(new Date(input_date).setHours(0,0,0)) })
-		that.changeUiState('INPUT')
-	}
-	else {
+	else
 		that.setState({ [input_name]: '' })
-		that.changeUiState('INPUT')
-	}
+	that.changeUiState('INPUT')
 }
 
 export default updateDayPicker
