cfe72aaf61a892fdf66a52e7f2f91f28a23928e1
Ahmad Forhat <ahmad.forhat@gmail.com>
2021-01-05 19:29:38 -0300

    chore: change validation date

--- src/SaleView/components/View/Select/compareDateToNow.js
+++ src/SaleView/components/View/Select/compareDateToNow.js
@@ -1,26 +1,26 @@
-const monthToEnglish = (month) => {
-	switch (month) {
-		case 'jan': return 'jan'
-		case 'fev': return 'feb'
-		case 'mar': return 'mar'
-		case 'abr': return 'apr'
-		case 'mai': return 'may'
-		case 'jun': return 'jun'
-		case 'jul': return 'jul'
-		case 'ago': return 'aug'
-		case 'set': return 'sep'
-		case 'out': return 'oct'
-		case 'nov': return 'nov'
-		case 'dez': return 'dec'
-	}
-}
+// const monthToEnglish = (month) => {
+// 	switch (month) {
+// 		case 'jan': return 'jan'
+// 		case 'fev': return 'feb'
+// 		case 'mar': return 'mar'
+// 		case 'abr': return 'apr'
+// 		case 'mai': return 'may'
+// 		case 'jun': return 'jun'
+// 		case 'jul': return 'jul'
+// 		case 'ago': return 'aug'
+// 		case 'set': return 'sep'
+// 		case 'out': return 'oct'
+// 		case 'nov': return 'nov'
+// 		case 'dez': return 'dec'
+// 	}
+// }
 
-const dateIsGreaterThanToday = (date) =>
-	new Date(new Date(date).setFullYear(new Date().getFullYear())) > new Date(new Date().toDateString())
+// const dateIsGreaterThanToday = (date) =>
+// 	new Date(new Date(date).setFullYear(new Date().getFullYear())) > new Date(new Date().toDateString())
 
 const compareDateToNow = (date) => {
-	const dateInEnglish = `${date.substring(0,3)}${monthToEnglish(date.substring(3))}`
-	return dateIsGreaterThanToday(dateInEnglish)
+	// const dateInEnglish = `${date.substring(0,3)}${monthToEnglish(date.substring(3))}`
+	return false
 }
 
 export default compareDateToNow
